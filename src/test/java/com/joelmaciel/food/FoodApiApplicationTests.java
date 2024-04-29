8307a7bf087e5d7605f694ed0d943829904e6add
Joel <joel.viana@audora.com.br>
2024-04-29 14:45:23 -0300

    first commit

---
+++ src/test/java/com/joelmaciel/food/FoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelmaciel.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
