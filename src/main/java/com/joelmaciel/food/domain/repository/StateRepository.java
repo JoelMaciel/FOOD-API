9878979a7b83d3aab2f632f4f96dbf106e84bef3
Joel <joel.viana@audora.com.br>
2024-05-01 13:36:30 -0300

    reafctor: refactoring repositories

--- src/main/java/com/joelmaciel/food/domain/repository/StateRepository.java
+++ src/main/java/com/joelmaciel/food/domain/repository/StateRepository.java
@@ -2,7 +2,9 @@ package com.joelmaciel.food.domain.repository;
 
 import com.joelmaciel.food.domain.model.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateRepository extends JpaRepository<State, Long> {
 
 
