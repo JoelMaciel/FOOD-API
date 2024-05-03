70ede9903ef34456e869dea568323ab69d8ced8d
Joel <joel.viana@audora.com.br>
2024-05-03 13:53:02 -0300

    feat:  mapping and creating order and order_item migration

---
+++ src/main/java/com/joelmaciel/food/domain/model/OrderItem.java
@@ -0,0 +1,31 @@
+package com.joelmaciel.food.domain.model;
+
+import com.joelmaciel.food.domain.Order;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class OrderItem {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+    private Integer quantity;
+    private String observation;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Product product;
+}
